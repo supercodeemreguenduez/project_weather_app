077704c0cd5c3498629a527c4f978da5d9038338
githubsupercode <alex.supercode@gmail.com>
2022-06-03 14:32:50 +0200

    alexbranch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // Geocode latitude-longitude API
 
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=Berlin&limit=5&appid=4d0b14ce24e912efc3f78767002e0237')
+fetch('http://api.openweathermap.org/geo/1.0/direct?q=Berlin,DE&limit=5&appid=4d0b14ce24e912efc3f78767002e0237')
     .then(response => response.json())
     .then(json => {
         console.log(json);
@@ -10,6 +10,12 @@ fetch('http://api.openweathermap.org/geo/1.0/direct?q=Berlin&limit=5&appid=4d0b1
 // weather API (needs Latitude Data)
 
 fetch('https://api.openweathermap.org/data/2.5/weather?lat=52.5170365&lon=13.3888599&appid=4d0b14ce24e912efc3f78767002e0237')
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+    })
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=04e1ab889dbc9ff41996c453df63349e')
     .then(response => response.json())
     .then(json => {
         console.log(json);
